3eb4f9bad808a7aae49d372a1253bf35e0af3abc
Ubuntu <ubuntu@ip-172-31-91-241.ec2.internal>
2019-10-25 00:46:18 +0000

    Criação de primeiro index haskell

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -1,13 +1,31 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE MultiParamTypeClasses
+{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TypeFamilies #-}
 module Handler.Home where
 
 import Import
-import Network.HTTP.Types.Status
+--import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
 
 getHomeR :: Handler Html
-getHomeR = undefined
+getHomeR = do 
+    defaultLayout $ do 
+        toWidgetHead[julius|
+            function ola(){
+                alert("HELLO!");
+            }
+        |]
+        
+        
+        toWidgetHead[lucius|
+            h1{
+                color:red;
+            }
+        |]
+        
+        [whamlet|
+            <h1>Hello World
+        |]

1b3988be54d45d5b969a1079d90a24fd0aec6aab
Ubuntu <ubuntu@ip-172-31-91-241.ec2.internal>
2019-10-31 23:16:04 +0000

    ads.txt

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ cd HaskellProject &&
 git pull origin master &&
 stack build &&
 lsof -i:80 -Fp | sed 's/^p//' | head -n -1 | xargs kill -9;
-nohup stack exec HaskellProject > /dev/null
+nohup stack exec aulahaskell > /dev/null
 echo "deploy finished"
 EOF

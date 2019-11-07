02aa2cc7c81f4e81d87f36790a87b4c0f57632e8
root <root@vps745440.ovh.net>
2019-11-07 23:55:20 +0100

    try

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 git add . &&
 git commit -m "$1" &&
 git push origin master &&
-ssh root@164.132.228.201 <<EOF
+ssh root@164.132.228.201 << EOF
 cd HaskellProject &&
 git pull origin master &&
 stack build &&

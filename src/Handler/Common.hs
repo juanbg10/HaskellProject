4362aa8fd1a893dcba58ae4e1ee11930767e4643
root <root@vps745440.ovh.net>
2019-12-05 21:15:18 +0100

    trying favicon

--- src/Handler/Common.hs
+++ src/Handler/Common.hs
@@ -15,7 +15,7 @@ import Import
 getFaviconR :: Handler TypedContent
 getFaviconR = do cacheSeconds $ 60 * 60 * 24 * 30 -- cache for a month
                  return $ TypedContent "image/x-icon"
-                        $ toContent $(embedFile "config/crabs.png")
+                        $ toContent $(embedFile "static/crabs.png")
 
 getRobotsR :: Handler TypedContent
 getRobotsR = return $ TypedContent typePlain
